e18bf944501a8d6f3f85c3e79734345b3a536071
Tom Wallis <github@tomwallis.net>
2017-03-28 12:28:42 +0100

    removing redundant bits

--- runtests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import theatre_ag
-import resp_base
-
-if __name__ == "__main__":
-    unittest.main.loader.TestLoader.discover(".",
-                                             pattern='test*.py',
-                                             top_level_dir=None)
--- theatre_ag_old/__init__.py
+++
@@ -1,3 +0,0 @@
-from .actor import Actor, Team
-from .workflow import Idling, default_cost
-from .clock import SynchronizingClock
